0474028fbee0ff6e2e79a220db769e4008a013c5
Concept <conceptcodes@gmail.com>
2021-11-21 23:31:42 -0500

    main endpoint created

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-const app = require('express')()
-const router = require('express').Router()
+// imports
+const express = require('express')
+const morgan = require('morgan')
+const { createCanvas, createImageData } = require('canvas')
+const app = express()
 
-router.get('/:size', (req, res) => {
-    const size = req.params.size;
-    console.log(size)
-})
+// logging
+app.use(morgan('dev'))
 
-app.listen(3000, () => {
-    console.log('listening on http://localhost:3000')
-})
+// main endpoint
+app.get("/:size", (req, res) => {
+    // get size dimensions 
+    const width = parseInt(req.params.size);
+    const height = parseInt(req.params.size);
 
+    // create the image
+    const canvas = createCanvas(width, height)
+    const ctx = canvas.getContext('2d')
+    ctx.fillStyle = '#808080' // set background to gray
+    ctx.fillRect(0, 0, width, height)
+
+    // convert canvas to image format and return to user
+    res.status(200).send({ width, height,  img: canvas.toDataURL() })
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
